cc3134e18df02269aa60aa8bc805417c5f6f9e92
mkmeden <midhupayathuparambil@gmail.com>
2024-06-28 16:26:47 +0530

    folder change of authpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import {Route , Routes} from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
-import AuthPage from './pages/HomePage/AuthPage/AuthPage';
+import AuthPage from './pages/AuthPage/AuthPage';
 
 import './App.css'
 
